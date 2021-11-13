3f9e2b97dbb6e8db79b3d5ec8273491d7ac154b4
Miłosz Mokrzycki <milosz.mokrzycki21@gmail.com>
2021-11-13 17:51:02 +0100

    add radio-buttons to form

--- src/components/AddVechicle.js
+++ src/components/AddVechicle.js
@@ -5,13 +5,23 @@ import { TextField } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import { DatePicker } from '@mui/lab';
 import { LocalizationProvider } from '@mui/lab';
+import { FormLabel } from '@mui/material';
+import { RadioGroup } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import { Radio } from '@mui/material';
+import { FormControl } from '@mui/material';
+
 
 const AddVechicle = () => {
 
+  const currentDate = new Date();
+  const minimumDate = new Date(1990, 1);
+
   const [carBrand, setCarBrand] = useState('');
   const [carModel, setCarModel] = useState('');
-  const [carProductionYear, setCarProductionYear] = useState(new Date());
+  const [carProductionYear, setCarProductionYear] = useState(currentDate);
   const [carCourse, setCarCourse] = useState('');
+  const [carFuelType, setCarFuelType] = useState('');
 
   const handleBrandChange = e => {
       
@@ -24,8 +34,8 @@ const AddVechicle = () => {
   }
 
   const handleCarProductionYearChange = e => {
-    setCarProductionYear(e)
-
+    
+    setCarProductionYear(e);
   }
 
   const handleCarCourseChange = e => {
@@ -37,7 +47,10 @@ const AddVechicle = () => {
     }
   }
 
-
+  const handleCarFuelTypeChange = e => {
+    
+    setCarFuelType(e.target.value);
+  }
 
   return ( 
     <Box component = "form"
@@ -83,6 +96,8 @@ const AddVechicle = () => {
           label="Rok produkcji"
           value={carProductionYear}
           onChange={handleCarProductionYearChange}
+          maxDate={currentDate}
+          minDate={minimumDate}
           renderInput={(params) => <TextField {...params} helperText={null} />}/>
     </LocalizationProvider>
 
@@ -106,7 +121,15 @@ const AddVechicle = () => {
         }}
 
             />
-      
+ <FormControl component="fieldset">
+  <FormLabel component="legend">Gender</FormLabel>
+  <RadioGroup aria-label="gender" name="row-radio-buttons-group">
+    <FormControlLabel value="female" control={<Radio />} label="Female" />
+    <FormControlLabel value="male" control={<Radio />} label="Male" />
+    <FormControlLabel value="other" control={<Radio />} label="Other" />
+ 
+  </RadioGroup>
+</FormControl>
    
     </div>
 
