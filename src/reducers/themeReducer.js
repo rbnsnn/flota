d327cb8950f09d6e575e01ee8e7bddcb1931d6b8
Milosz <milosz.mokrzycki21@gmail.com>
2021-11-14 12:15:17 +0100

    theme change fix

--- src/reducers/themeReducer.js
+++ src/reducers/themeReducer.js
@@ -3,8 +3,6 @@ import { THEME_CHANGE } from "../actions/appActions"
 export const themeReducer = (state = { isDark: false, mode: 'light' }, action) => {
     switch (action.type) {
         case THEME_CHANGE:
-            console.log('dziala');
-
             const isDark = !state.isDark;
             let mode = 'light';
 
@@ -14,19 +12,14 @@ export const themeReducer = (state = { isDark: false, mode: 'light' }, action) =
                 mode = 'light';
             }
 
-            console.log(state)
-
             state = {
                 isDark,
                 mode
             }
 
-
             return state;
 
-
         default:
-
             return state;
     }
 
