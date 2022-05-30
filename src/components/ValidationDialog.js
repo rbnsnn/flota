3a216d3c73b1c0f8f7805776d1bebe38a7616dea
Milosz <57958312+rbnsnn@users.noreply.github.com>
2022-05-30 22:16:38 +0200

    edit update

---
+++ src/components/ValidationDialog.js
@@ -0,0 +1,38 @@
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import { forwardRef } from 'react';
+import { Slide } from '@mui/material';
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const ValidationDialog = ({ isOpen, handleDialog }) => {
+
+    return (
+        <div>
+            <Dialog
+                TransitionComponent={Transition}
+                keepMounted
+                open={isOpen}
+                onClose={handleDialog}
+                aria-describedby="Validation-dialog-description"
+            >
+                <DialogContent id="Validation-dialog-description">
+                    Proszę uzupełnić wszystkie pola!
+                </DialogContent>
+
+                <DialogActions>
+
+                    <Button onClick={handleDialog} autoFocus>
+                        Zamknij
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default ValidationDialog
